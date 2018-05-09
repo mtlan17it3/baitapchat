5609a58937903df298b6b99a33acb538b369ea7f
mtlan17it3 <38147842+mtlan17it3@users.noreply.github.com>
2018-05-09 07:43:01 +0700

    Add files via upload

---
+++ server.java
@@ -0,0 +1,36 @@
+package chat;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+	public static void main(String args[]){
+	try {
+		ServerSocket sv = new ServerSocket(8014);
+		Socket s = sv.accept();
+		DataInputStream din = new DataInputStream(s.getInputStream());
+		DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = "", str2 = "";
+		while (!str.equals("stop")) {
+			str = din.readUTF();
+			System.out.println("client says: " + str);
+			str2 = br.readLine();
+			dout.writeUTF(str2);
+			dout.flush();
+		}
+		din.close();
+		s.close();
+		sv.close();
+	} catch (Exception e) {
+		// TODO: handle exception
+	}
+	
+}
+}
+
